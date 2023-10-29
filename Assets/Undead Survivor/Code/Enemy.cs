91e3f006822106f0b29b0350d6828640ce09238a
Ku bonsang <bonsang05r@gmail.com>
2023-10-29 18:07:22 +0900

    3주차 (몬스터 레벨 적용까지)
    4주차 평일까지 3주차 완성 후, 주말에 4주차를 할 예정입니다.

--- Assets/Undead Survivor/Code/Enemy.cs
+++ Assets/Undead Survivor/Code/Enemy.cs
@@ -5,17 +5,22 @@ using UnityEngine;
 public class Enemy : MonoBehaviour
 {
     public float speed;
+    public float health;
+    public float maxHealth;
+    public RuntimeAnimatorController[] animCon;
     public Rigidbody2D target;
 
-    bool isLive = true;
+    bool isLive;
 
     Rigidbody2D rigid;
+    Animator anim;
     SpriteRenderer spriter;
 
     private void Awake()
     {
         rigid = GetComponent<Rigidbody2D>();
         spriter = GetComponent<SpriteRenderer>();
+        anim = GetComponent<Animator>();
     }
 
     private void FixedUpdate()
@@ -33,5 +38,20 @@ public class Enemy : MonoBehaviour
         if (!isLive)
             return;
         spriter.flipX = target.position.x < rigid.position.x;
+    }
+
+    private void OnEnable()
+    {
+        target = GameManager.instance.player.GetComponent<Rigidbody2D>();
+        isLive = true;
+        health = maxHealth;
+    }
+
+    public void Init(SpawnData data)
+    {
+        anim.runtimeAnimatorController = animCon[data.spriteType];
+        speed = data.speed;
+        maxHealth = data.health;
+        health = data.health;
     }
 }
--- Assets/Undead Survivor/Code/GameManager.cs
+++ Assets/Undead Survivor/Code/GameManager.cs
@@ -7,6 +7,11 @@ public class GameManager : MonoBehaviour
 {
     //다른 클래스에서 접근할 수 있도록 public static으로 선언
     public static GameManager instance;
+
+    public float gameTime;
+    public float maxGameTime = 2 * 10f;
+
+    public PoolManager pool;
     public Player player;
 
     private void Awake()
@@ -14,4 +19,16 @@ public class GameManager : MonoBehaviour
         //현재 생성된 인스턴스 할당
         instance = this;
     }
+
+    private void Update()
+    {
+        gameTime += Time.deltaTime;
+
+        if(gameTime > maxGameTime)
+        {
+            gameTime = maxGameTime;
+        }
+
+    }
+
 }
